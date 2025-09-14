14417f16da39d44f3e53b7e7647ffacd6a36583a
Ndricim <ndricimrr@gmail.com>
2025-09-14 03:30:03 +0200

    Add translations contributions page

--- webapp/js/translations.js
+++ webapp/js/translations.js
@@ -58,6 +58,73 @@ const translations = {
 
     // Week Indicator
     "week.prefix": "Week",
+
+    // Privacy Policy
+    "privacy.title": "Privacy Policy",
+    "privacy.intro":
+      'This Privacy Policy applies to the mobile application BilMenu Bilkent Cafeteria ("App") provided by "Ndricim Rrapi" as a developer. This policy describes how we handle information we collect from users of the App.',
+    "privacy.no_collect_title": "Information We Don't Collect",
+    "privacy.no_collect_text":
+      "We do not collect any personally identifiable information from users of our App. We do not use cookies, analytics tools, or any other means to track users' online activities.",
+    "privacy.contact_title": "Contact Us",
+    "privacy.contact_text":
+      "If you have any questions about this Privacy Policy, please contact us at",
+    "privacy.data_source_title": "Data Source",
+    "privacy.data_source_text":
+      "Our App simply visualizes data sourced from another website. This data is used solely for the purpose of providing users with information within the App.",
+    "privacy.third_party_title": "Third-Party Links",
+    "privacy.third_party_text":
+      "Our App may contain links to third-party websites. Please be aware that we are not responsible for the privacy practices of such other sites. We encourage users to read the privacy statements of each website that collects personally identifiable information.",
+    "privacy.contact_title": "Contact Us",
+
+    // Contributions
+    "contrib.title": "Contributions",
+    "contrib.intro":
+      "Thank you for considering contributing to our open-source project! Here are some ways you can help:",
+    "contrib.how_title": "How to Contribute",
+    "contrib.images_title": "Add missing images",
+    "contrib.images_text":
+      "Help us complete our meal image database by adding photos of meals that are currently missing.",
+    "contrib.bugs_title": "Report bugs",
+    "contrib.bugs_text": "Found a bug? Let us know so we can fix it!",
+    "contrib.features_title": "Suggest features",
+    "contrib.features_text":
+      "Have an idea for a new feature? We'd love to hear it!",
+    "contrib.images_step":
+      "Collect more images of the meals by taking pictures at the cafeteria and uploading them to the project. Check steps",
+    "contrib.images_here": "here",
+    "contrib.code_title": "Contribute to code",
+    "contrib.code_1":
+      "Help improve the quality of code by submitting pull requests.",
+    "contrib.code_2":
+      "Contribute to the improvement of the UX (User Experience) of the site.",
+    "contrib.code_3":
+      "Assist with keeping the parsing algorithm up to date if the source website changes its design.",
+    "contrib.code_4":
+      "Address any other issues or improvements that come to your mind.",
+    "contrib.code_steps": "Check steps",
+    "contrib.code_here": "here",
+    "contrib.getting_started_title": "Getting Started",
+    "contrib.getting_started_text":
+      "If you're interested in contributing, please follow these steps:",
+    "contrib.step_1": "Fork the repository",
+    "contrib.step_2": "Create a new branch for your feature or bugfix",
+    "contrib.step_3": "Make your changes",
+    "contrib.step_4": "Test your changes",
+    "contrib.step_5": "Submit a pull request",
+    "contrib.thank_you":
+      "Thank you for your interest in contributing to our project!",
+    "contrib.issues_title": "Issues",
+    "contrib.issues_text":
+      "If you want to contribute but don't know where to start, check the",
+    "contrib.issues_link": "Issues",
+    "contrib.issues_text2":
+      "section on GitHub. You can create a new issue to propose new features or report bugs.",
+    "contrib.issues_review":
+      "After creating an issue, it will be reviewed, and you'll be given the green light to proceed with your contribution.",
+    "contrib.contact_title": "Contact",
+    "contrib.contact_text":
+      "If you have any questions or need assistance, please feel free to contact us at",
   },
 
   tr: {
@@ -109,7 +176,7 @@ const translations = {
     "modal.contribute": "Şimdi Katkıda Bulun",
 
     // Footer
-    "footer.parsed": "Veriler şuradan",
+    "footer.parsed": "Veriler",
     "footer.source": "Bilkent Üniversitesi Kafeterya Sayfası",
 
     // Language Switcher
@@ -118,6 +185,74 @@ const translations = {
 
     // Week Indicator
     "week.prefix": "Hafta",
+
+    // Privacy Policy
+    "privacy.title": "Gizlilik Politikası",
+    "privacy.intro":
+      'Bu Gizlilik Politikası, geliştirici "Ndricim Rrapi" tarafından sağlanan BilMenu Bilkent Kafeterya ("Uygulama") mobil uygulaması için geçerlidir. Bu politika, Uygulama kullanıcılarından topladığımız bilgileri nasıl ele aldığımızı açıklar.',
+    "privacy.no_collect_title": "Toplamadığımız Bilgiler",
+    "privacy.no_collect_text":
+      "Uygulamamızın kullanıcılarından kişisel olarak tanımlanabilir herhangi bir bilgi toplamıyoruz. Kullanıcıların çevrimiçi etkinliklerini izlemek için çerezler, analitik araçlar veya başka herhangi bir araç kullanmıyoruz.",
+    "privacy.contact_title": "Bizimle İletişime Geçin",
+    "privacy.contact_text":
+      "Bu Gizlilik Politikası hakkında herhangi bir sorunuz varsa, lütfen bizimle iletişime geçin:",
+    "privacy.data_source_title": "Veri Kaynağı",
+    "privacy.data_source_text":
+      "Uygulamamız, başka bir web sitesinden alınan verileri yalnızca görselleştirir. Bu veriler yalnızca kullanıcılara Uygulama içinde bilgi sağlama amacıyla kullanılır.",
+    "privacy.third_party_title": "Üçüncü Taraf Bağlantıları",
+    "privacy.third_party_text":
+      "Uygulamamız üçüncü taraf web sitelerine bağlantılar içerebilir. Lütfen bu tür diğer sitelerin gizlilik uygulamalarından sorumlu olmadığımızı unutmayın. Kullanıcıları, kişisel olarak tanımlanabilir bilgi toplayan her web sitesinin gizlilik beyanlarını okumaya teşvik ediyoruz.",
+    "privacy.contact_title": "Bizimle İletişime Geçin",
+
+    // Contributions
+    "contrib.title": "Katkılar",
+    "contrib.intro":
+      "Açık kaynak projemize katkıda bulunmayı düşündüğünüz için teşekkürler! Yardımcı olabileceğiniz bazı yollar:",
+    "contrib.how_title": "Nasıl Katkıda Bulunabilirsiniz",
+    "contrib.images_title": "Eksik görselleri ekleyin",
+    "contrib.images_text":
+      "Şu anda eksik olan yemek fotoğraflarını ekleyerek yemek görsel veritabanımızı tamamlamamıza yardımcı olun.",
+    "contrib.bugs_title": "Hata bildirin",
+    "contrib.bugs_text":
+      "Bir hata buldunuz mu? Düzeltebilmemiz için bize bildirin!",
+    "contrib.features_title": "Özellik önerin",
+    "contrib.features_text":
+      "Yeni bir özellik için fikriniz var mı? Duymayı çok isteriz!",
+    "contrib.images_step":
+      "Kafeteryada fotoğraf çekerek ve bunları projeye yükleyerek daha fazla yemek görseli toplayın. Adımları kontrol edin",
+    "contrib.images_here": "burada",
+    "contrib.code_title": "Koda katkıda bulunun",
+    "contrib.code_1":
+      "Pull request göndererek kod kalitesinin iyileştirilmesine yardımcı olun.",
+    "contrib.code_2":
+      "Sitenin UX (Kullanıcı Deneyimi) iyileştirmesine katkıda bulunun.",
+    "contrib.code_3":
+      "Kaynak web sitesi tasarımını değiştirirse ayrıştırma algoritmasını güncel tutmaya yardımcı olun.",
+    "contrib.code_4":
+      "Aklınıza gelen diğer sorunları veya iyileştirmeleri ele alın.",
+    "contrib.code_steps": "Adımları kontrol edin",
+    "contrib.code_here": "burada",
+    "contrib.getting_started_title": "Başlarken",
+    "contrib.getting_started_text":
+      "Katkıda bulunmakla ilgileniyorsanız, lütfen şu adımları izleyin:",
+    "contrib.step_1": "Depoyu çatallayın",
+    "contrib.step_2":
+      "Özelliğiniz veya hata düzeltmeniz için yeni bir dal oluşturun",
+    "contrib.step_3": "Değişikliklerinizi yapın",
+    "contrib.step_4": "Değişikliklerinizi test edin",
+    "contrib.step_5": "Pull request gönderin",
+    "contrib.thank_you": "Projemize katkıda bulunma ilginiz için teşekkürler!",
+    "contrib.issues_title": "Sorunlar",
+    "contrib.issues_text":
+      "Katkıda bulunmak istiyorsanız ama nereden başlayacağınızı bilmiyorsanız, GitHub'daki",
+    "contrib.issues_link": "Sorunlar",
+    "contrib.issues_text2":
+      "bölümünü kontrol edin. Yeni özellikler önerebilir veya hata bildirebilirsiniz.",
+    "contrib.issues_review":
+      "Bir sorun oluşturduktan sonra, incelenecek ve katkınızla devam etmeniz için yeşil ışık verilecektir.",
+    "contrib.contact_title": "İletişim",
+    "contrib.contact_text":
+      "Herhangi bir sorunuz varsa veya yardıma ihtiyacınız varsa, lütfen bizimle iletişime geçmekten çekinmeyin:",
   },
 };
 
