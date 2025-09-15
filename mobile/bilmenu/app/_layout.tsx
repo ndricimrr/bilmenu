51a195d34ccbe64f58073b41eabda8ce95e6b55f
Ndricim <ndricimrr@gmail.com>
2025-09-15 21:37:25 +0200

    remove unused deps

--- mobile/bilmenu/app/_layout.tsx
+++ mobile/bilmenu/app/_layout.tsx
@@ -5,7 +5,6 @@ import {
 } from "@react-navigation/native";
 import { Stack } from "expo-router";
 import { StatusBar } from "expo-status-bar";
-import "react-native-reanimated";
 
 import { useColorScheme } from "@/hooks/use-color-scheme";
 import { LanguageProvider } from "@/contexts/LanguageContext";
