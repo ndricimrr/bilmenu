88cb49554099c7efb2323d8d75ed8a0eab197056
ndricimrr <ndricimrr@gmail.com>
2025-10-23 21:16:42 +0200

    Fix race condition on page load

--- webapp/js/fetchMealPlan.js
+++ webapp/js/fetchMealPlan.js
@@ -1,5 +1,22 @@
 // This file is used to fetch the JSON data for the current day. To be then used in the index.html
 var weeklyMealPlan = {};
+var mealPlanLoadCallbacks = [];
+var mealPlanLoaded = false;
+var mealPlanLoadFailed = false;
+
+// Register callback to be called when meal plan loads
+function onMealPlanLoad(callback) {
+  if (mealPlanLoaded) {
+    // Already loaded, call immediately
+    callback(true);
+  } else if (mealPlanLoadFailed) {
+    // Already failed, call immediately
+    callback(false);
+  } else {
+    // Register for when it loads
+    mealPlanLoadCallbacks.push(callback);
+  }
+}
 
 function loadJSON(filename, callback) {
   fetch(filename)
@@ -22,7 +39,15 @@ loadJSON(getCurrentWeekFileName(), function (jsonObject) {
   if (jsonObject) {
     weeklyMealPlan = window.Week.fromJSON(jsonObject).days;
     console.log("Meal Plan Loaded.", weeklyMealPlan);
+    mealPlanLoaded = true;
+    // Notify all waiting callbacks
+    mealPlanLoadCallbacks.forEach((cb) => cb(true));
+    mealPlanLoadCallbacks = [];
   } else {
     console.log("Failed to load JSON file from " + getCurrentWeekFileName());
+    mealPlanLoadFailed = true;
+    // Notify all waiting callbacks of failure
+    mealPlanLoadCallbacks.forEach((cb) => cb(false));
+    mealPlanLoadCallbacks = [];
   }
 });
