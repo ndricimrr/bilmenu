98b4fe9b74a4576272bcb175b6b8246fd84137c5
ndricimrr <ndricimrr@gmail.com>
2026-07-17 21:14:04 +0200

    Fix isVegan not showing

--- webapp/js/meal.js
+++ webapp/js/meal.js
@@ -2,9 +2,13 @@ const isNodeEnvironmentMeal =
   typeof module !== "undefined" && typeof module.exports !== "undefined";
 
 class Meal {
-  constructor(tr, en) {
+  constructor(tr, en, isVegan) {
     this._tr = tr;
     this._en = en;
+    this._isVegan =
+      typeof isVegan === "boolean"
+        ? isVegan
+        : /vegan/i.test(`${tr || ""} ${en || ""}`);
   }
 
   get tr() {
@@ -23,17 +27,26 @@ class Meal {
     this._en = en;
   }
 
+  get isVegan() {
+    return this._isVegan;
+  }
+
+  set isVegan(isVegan) {
+    this._isVegan = !!isVegan;
+  }
+
   // Convert the Meal instance to a JSON-friendly format
   toJSON() {
     return {
       tr: this._tr,
       en: this._en,
+      isVegan: this._isVegan,
     };
   }
 
   // Create a Meal instance from a JSON object
   static fromJSON(json) {
-    return new Meal(json.tr, json.en);
+    return new Meal(json.tr, json.en, json.isVegan);
   }
 }
 
